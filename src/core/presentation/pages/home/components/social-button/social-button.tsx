4f85ff799b234801b1e9563c2988f0537dacc3eb
viniciusanchieta <vinicius.anchieta@outlook.com>
2023-03-17 14:12:56 -0300

    feat: add hover animation in buttons

--- src/core/presentation/pages/home/components/social-button/social-button.tsx
+++ src/core/presentation/pages/home/components/social-button/social-button.tsx
@@ -11,12 +11,12 @@ function SocialButtonComponent({ url, icon, title }: SocialButtonProps) {
   return (
     <button
       onClick={handleClick}
-      className='flex [&>svg]:fill-gray-100 text-gray-100 border-gray-100 border py-3 items-center gap-2 w-210px justify-center rounded-md'
+      className='flex [&>svg]:fill-gray-100 text-gray-100 border-gray-100 border py-3 items-center gap-2 w-210px justify-center rounded-md hover:bg-gray-100 hover:text-main hover:shadow-md [&>svg]:duration-300 [&>svg]:hover:duration-300 [&>svg]:hover:fill-main transition-all duration-300 ease-in-out'
       role='button'
       aria-label={`Navigate to ${title}`}
     >
       {icon}
-      <span className='text-gray-100 text-base'>{title}</span>
+      <span className='text-base'>{title}</span>
     </button>
   );
 }
