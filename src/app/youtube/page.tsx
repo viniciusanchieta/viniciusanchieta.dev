7272541cad9e9dc3d1dcabb156164da295215b3f
viniciusanchieta <vinicius.anchieta@outlook.com>
2023-06-11 12:15:21 -0300

    feat: alter url of youtube to mobile youtube

--- src/app/youtube/page.tsx
+++ src/app/youtube/page.tsx
@@ -4,6 +4,6 @@ import { useEffect } from 'react';
 
 export default function Page() {
   useEffect(() => {
-    window.location.href = 'https://www.youtube.com/@viniciusanchieta';
+    window.location.href = 'https://m.youtube.com/@viniciusanchieta';
   }, []);
 }
