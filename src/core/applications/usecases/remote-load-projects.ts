91cb6285df4026b208a85e745f30cf0828582a9d
viniciusanchieta <vinicius.anchieta@outlook.com>
2023-11-04 00:02:22 -0300

    feat: set url of load experiences and projects in .env file

--- src/core/applications/usecases/remote-load-projects.ts
+++ src/core/applications/usecases/remote-load-projects.ts
@@ -5,7 +5,7 @@ export class RemoteLoadProjects implements LoadProjects {
   async run(): Promise<LoadProjects.Response> {
     const httpResponse = await fetchApi({
       method: 'GET',
-      url: 'https://gist.githubusercontent.com/viniciusanchieta/789c71fb0128dea5140f98ee29cedce5/raw/680e9f839e4263bffb8f23a69b45a7e3a7b4c04a/my-projects.json'
+      url: process.env.API_LOAD_PROJECTS as string
     });
     const response = httpResponse as LoadProjects.Response;
 
